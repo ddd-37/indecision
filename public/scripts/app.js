e702d66a5d036fdd4c6583ca864cdaaf3801b51a
Devon Deason <devon.d.deason@gmail.com>
2019-09-29 09:22:45 -0600

    ugh, mead goes so slow!

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -4,11 +4,54 @@ console.log("apfp");
 
 /// JSX JavaScript XML
 var template = React.createElement(
-  "p",
+  "div",
   null,
-  "This is JSX from app.js!"
+  React.createElement(
+    "h1",
+    null,
+    "Indescision App"
+  ),
+  React.createElement(
+    "p",
+    null,
+    "This is some info"
+  ),
+  React.createElement(
+    "ol",
+    null,
+    React.createElement(
+      "li",
+      null,
+      "List Item 1"
+    ),
+    React.createElement(
+      "li",
+      null,
+      "List Item 2"
+    )
+  )
+);
+
+var templateTwo = React.createElement(
+  "div",
+  null,
+  React.createElement(
+    "h1",
+    null,
+    "Devon Deason"
+  ),
+  React.createElement(
+    "p",
+    null,
+    "Age: 37"
+  ),
+  React.createElement(
+    "p",
+    null,
+    "Location: Denver"
+  )
 );
 
 var appRoot = document.getElementById("app");
 
-ReactDOM.render(template, appRoot);
+ReactDOM.render(templateTwo, appRoot);
--- src/app.js
+++ src/app.js
@@ -1,8 +1,25 @@
 console.log("apfp");
 
 /// JSX JavaScript XML
-var template = <p>This is JSX from app.js!</p>;
+var template = (
+  <div>
+    <h1>Indescision App</h1>
+    <p>This is some info</p>
+    <ol>
+      <li>List Item 1</li>
+      <li>List Item 2</li>
+    </ol>
+  </div>
+);
+
+var templateTwo = (
+  <div>
+    <h1>Devon Deason</h1>
+    <p>Age: 37</p>
+    <p>Location: Denver</p>
+  </div>
+);
 
 var appRoot = document.getElementById("app");
 
-ReactDOM.render(template, appRoot);
+ReactDOM.render(templateTwo, appRoot);
